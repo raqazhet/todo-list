085fa6b633c7faeebb5bccf79c80e8e4b7ea022a
raqazhet <qazhetrazaq@gmail.com>
2023-06-01 21:20:13 +0600

    postgres add

--- cmd/main.go
+++ cmd/main.go
@@ -8,14 +8,26 @@ import (
 	"todolist/pkg/service"
 
 	"todolist"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := InitConfig(); err != nil {
+		log.Printf("initConfig err: %v", err)
+		return
+	}
 	repos := repository.NewRepository()
 	service := service.NewService(*repos)
 	handlers := handler.NewHandler(service)
 	srv := new(todolist.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %v", err)
 	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("./configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
